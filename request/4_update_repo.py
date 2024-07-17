cf27bce665feae77805e37adc4976595b605421d
GradPolina <Gradinar_polina@yahoo.com>
2024-07-17 12:19:26 -0500

    Done

---
+++ request/4_update_repo.py
@@ -0,0 +1,13 @@
+import requests
+def update_repo(url):
+    data = {'description': 'I know Python Requests!'}
+    headers = {'Authorization': f'Bearer {token}', 'Accept': 'application/vnd.github+json'}
+    response = requests.patch(url, headers=headers, json=data)
+    print(f"Response status code: {response.status_code}")
+    return response.json()
+
+if __name__=='__main__':
+    url = 'https://api.github.com/repos/GradPolina/repo-created-with-api'
+    token = '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'
+    repo = update_repo(url)
+    assert repo['description'] == 'I know Python Requests!'
