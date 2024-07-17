9ec9b68224f8cad2e36a6089d5abad49760ba2e6
GradPolina <Gradinar_polina@yahoo.com>
2024-07-16 22:46:15 -0500

    Done

--- request/2_create_repo.py
+++ request/2_create_repo.py
@@ -1,13 +1,19 @@
 import requests
 from pprint import pprint
 def create_repo(url):
-    token = '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'
-    headers = {'Authorization': f'Bearer {token}'}
-    data = {'name': 'repo-created-with-api', 'private': True, 'has_wiki': False}
+    token = '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'
+    headers = {
+        'Authorization': f'Bearer {token}'}
+    data = {
+        'name': 'repo-created-with-api',
+        'private': True,
+        'has_wiki': False
+    }
+
     response = requests.post(url, headers=headers, json=data)
     print(f"Response status code: {response.status_code}")
-    print(response.json())
     return response.json()
+
 
 if __name__=='__main__':
     url = 'https://api.github.com/user/repos'
