2dcfcf78553424e5504738ea5a57e346563cb362
GradPolina <Gradinar_polina@yahoo.com>
2024-07-17 12:31:35 -0500

    Done

---
+++ request/5_delete_repo.py
@@ -0,0 +1,10 @@
+import requests
+def delete_repo(url):
+    headers = {'Authorization': f'Bearer {token}', 'Accept': 'application/vnd.github+json'}
+    response = requests.delete(url, headers=headers)
+    print(f"Response status code: {response.status_code}")
+    return response.json()
+if __name__=="__main__":
+    url = f'https://api.github.com/repos/GradPolina/repo-created-with-api'
+    token = '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'
+    delete_repo(url)
